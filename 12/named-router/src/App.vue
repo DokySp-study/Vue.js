c6acb656b741522b1a7e8ac5da69f34207a91a01
DokyoonKim <uhug@naver.com>
2019-07-27 10:50:14 +0900

    ch12. VueRouter: Duplicated router, Named router

---
+++ 12/named-router/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+
+    <div id="header">
+      <h1 id="headerText">ARIES LCC</h1>
+      <nav>
+        <ul>
+          <!-- <li><router-link to="/home">Home</router-link></li>
+          <li><router-link to="/about">About</router-link></li>
+          <li><router-link to="/Contacts">Contacts</router-link></li> -->
+          <li><router-link :to="{name:'home'}">Home</router-link></li>
+          <li><router-link :to='{name:"about"}'>About</router-link></li>
+          <li><router-link :to="{name:'contacts'}">Contacts</router-link></li>
+          <!-- name속성으로 바인딩해주면 따로 주소를 써 줄 필요가 없다! -->
+        </ul>
+      </nav>
+    </div>
+
+    <div id="container">
+      <router-view></router-view>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Contacts from './components/Contact.vue'
+import About from "./components/About.vue"
+import Home from "./components/Home.vue"
+import VueRouter from 'vue-router'
+import ContactByNo from './components/ContactByNo.vue';
+
+
+const router = new VueRouter({
+  routes: [
+    {path: "/", component: Home},
+    {path: "/home", name:"home", component: Home},
+    {path: "/about", name:"about",component: About},
+    {
+      path: "/contacts",
+      name: "contacts",
+      component: Contacts,
+      children : [
+        {path: ":no", name:"contactbyno",component: ContactByNo},
+      ]
+    },
+    // {path: "/contact/:no", component: ContactByNo},
+  ]
+})
+
+export default {
+  name: 'app',
+  router  // not a component!! (Vue Obj)
+}
+</script>
+
+<style>
+
+</style>
---
+++ 12/named-router/src/components/Contact.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <h1>
+            Contacts
+        </h1>
+        <div class="wrapper">
+            <div class="box" v-for="item in contacts" :key="item.no">
+                <!-- <router-link :to="/contacts/+item.no">{{ item.name }}</router-link> -->
+                <router-link :to="{name: 'contactbyno', params: {no: item.no}}">{{item.name}}</router-link>
+            </div>
+        </div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import contactlist from '../ContactList'
+
+export default {
+    name: "contact",
+    data() {
+        return {
+            contacts: contactlist.contacts 
+        }
+    },
+}
+</script>
+
+<style>
+.wrapper {
+    background-color: #fff;
+    clear: both;
+    display: table;
+}
+
+.box {
+    float: left;
+    background-color: aqua;
+    border-radius: 5px;
+    padding: 10px;
+    margin: 3px;
+    text-align: center;
+    font-size: 120%;
+    width: 100px;
+    font-weight: bold;
+}
+
+a:link, a:visited {
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+}
+</style>
